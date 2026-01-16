9b9908c1ad0e823906a03e10563a067aa8b3e2d8
Diego Gonzalez <diegoagonzalez33@gmail.com>
2026-01-16 18:57:26 -0300

    fix: /db not found

--- core/app/Http/Controllers/ProductController.php
+++ core/app/Http/Controllers/ProductController.php
@@ -6,6 +6,9 @@ use App\Models\LogStock;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Database\QueryException;
 
 
 class ProductController extends Controller
